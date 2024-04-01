1b507864ea11e1a93bf04cb937703950f2aa855b
rishikesh <rishikesh2715@gmail.com>
2024-04-01 17:10:16 -0500

    changed few things

--- MLP_Pattern_Recognition.py
+++ MLP_Pattern_Recognition.py
@@ -1,23 +1,18 @@
 import os
-import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 
-# Correctly list files in the directory
-directory_path = os.path.expanduser("~/Documents/Adaptive_Learning_System/Data")
+directory_path = os.path.expanduser("~/Documents/Adaptive_Learning_System/Data/")
 file_names = os.listdir(directory_path)
 
-arrays = {}
+features = []
 
-# Iterate over each file in the directory
 for file_name in file_names:
     if file_name.endswith(".txt"):
-        # Correctly join the directory path and the file name to get the full path
         file_path = os.path.join(directory_path, file_name)
-        # Read the array from the file
-        array = np.genfromtxt(file_path)
-        # Store the array in the dictionary, keying it by the file name without '.txt'
-        arrays[file_name[:-4]] = array
-        # print("hello")
-        print(file_name[:-4])
+        array = np.genfromtxt(file_path, dtype='int')
+        features.append(array)
+        # print(array)
+        print(array.shape)
+features = np.array(features)
+# print(features)
 
